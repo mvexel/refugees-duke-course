6f9e5125d4d40d7924bdf0d959ed3302facf3cc6
Martijn van Exel <m@rtijn.org>
2018-12-21 16:56:22 -0700

    progress week 3

--- course4/week3/interface_random_starter/AbstractMarkovModel.java
+++ course4/week3/interface_random_starter/AbstractMarkovModel.java
@@ -19,7 +19,23 @@ public abstract class AbstractMarkovModel implements IMarkovModel {
     public void setTraining(String s) {
         myText = s.trim();
     }
- 
+
+    public ArrayList<String> getFollows(String key) {
+        int pos = 0;
+        ArrayList<String> follows = new ArrayList();
+        while (true) {
+            int index = myText.indexOf(key, pos);
+            if (index > -1 && index < myText.length() - key.length()) {
+                follows.add(myText.substring(index + key.length(), index + key.length() + 1));
+                pos = index + 1;
+            } else {
+                break;
+            }
+        }
+        return follows;
+    }
+    
+    
     abstract public String getRandomText(int numChars);
 
 }
